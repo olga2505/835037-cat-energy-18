2cae98b3f44052d3d1707b14a2f263bc612a2855
Olga <151590@mail.ru>
2019-12-12 16:34:06 +0300

    Проблемы со спрайтом и котом 2

--- source/js/contact.js
+++ source/js/contact.js
@@ -5,19 +5,11 @@ var weight = form.querySelector("[name=weight]");
 var email = form.querySelector("[name=email]");
 var tel = form.querySelector("[name=tel]");
 
-// var submit = document.querySelector(".form__important");
 
 
 form.addEventListener("submit", function (evt) {
   if (!name.value || !weight.value || !email.value || !tel.value) {
     evt.preventDefault();
     console.log("Нужно заполнить все обязательные поля");
-  } 
+  }
 });
-
-// submit.addEventListener("submit", function (evt) {
-//   if (!name.value || !weight.value || !email.value || !tel.value) {
-//         evt.preventDefault();
-//         console.log("Нужно заполнить все обязательные поля");
-//       }
-//     });
