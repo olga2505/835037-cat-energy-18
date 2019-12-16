cf3687374bb8c1df13c378714ea6aeee707d9e17
Olga <151590@mail.ru>
2019-12-16 20:14:50 +0300

    оптимизация векторной и растровой графики

--- source/js/contact.js
+++ source/js/contact.js
@@ -1,6 +1,6 @@
 var form = document.querySelector(".form__program");
 
-var name = form.querySelector("[name=cat-name]");
+var catName = form.querySelector("[name=cat-name]");
 var weight = form.querySelector("[name=weight]");
 var email = form.querySelector("[name=email]");
 var tel = form.querySelector("[name=tel]");
@@ -8,7 +8,7 @@ var tel = form.querySelector("[name=tel]");
 
 
 form.addEventListener("submit", function (evt) {
-  if (!name.value || !weight.value || !email.value || !tel.value) {
+  if (!catName.value || !weight.value || !email.value || !tel.value) {
     evt.preventDefault();
     console.log("Нужно заполнить все обязательные поля");
   }
