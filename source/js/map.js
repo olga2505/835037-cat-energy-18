45d18598450acd25463fd36c61852c74793e6567
Olga <151590@mail.ru>
2020-01-13 19:48:45 +0300

    Внесение правок

--- source/js/map.js
+++ source/js/map.js
@@ -1,5 +1,3 @@
 var mapIframe = document.querySelector(".map__iframe");
-var mapStatic = document.querySelector(".map__static");
 
 mapIframe.classList.remove("map__iframe--nojs");
-mapStatic.classList.add("map__static--none");
